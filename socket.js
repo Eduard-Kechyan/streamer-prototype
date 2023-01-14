cee039ad83d96bdbf05197fc417356dce372c61f
Your Name <example@example.com>
2023-01-14 20:14:20 +0400

    Changing cors origin to the correct string

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 module.exports = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "https://streamer-prototype-backend.onrender.com"
+            origins: "*",
         }
     });
 
